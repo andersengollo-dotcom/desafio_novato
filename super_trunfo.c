08aa727e1e13445191957c6435555c41682e440d
andersengollo-dotcom <andersengollo@gmail.com>
2025-09-07 03:32:58 +0000

    Desafio Super Trunfo - Nível Novato

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,44 +2,89 @@
 
 int main(){
 
-char letra_estado;
-char codigo_da_carta[50];
-char nome_da_cidade[50];
-int populacao;
-float area;
-float PIB;
-int pontos_turisticos;
-printf("Digite a letra do estado: \n");
-scanf("%c", &letra_estado);
+char letra_estado1;
+char codigo_da_carta1[50];
+char nome_da_cidade1[50];
+int populacao1;
+float area1;
+float PIB1;
+int pontos_turisticos1;
+
+
+char letra_estado2;
+char codigo_da_carta2[50];
+char nome_da_cidade2[50];
+int populacao2;
+float area2;
+float PIB2;
+int pontos_turisticos2;
+
+printf(">>Insira os dados da CARTA 1:\n");
+printf("Digite a letra do estado:\n");
+scanf(" %c", &letra_estado1);
 
 printf("Digite o código da Carta: \n");
-scanf("%s", &codigo_da_carta);
+scanf("%s", &codigo_da_carta1);
 
 printf("Digite o Nome da cidade: \n");
-scanf("%s", &nome_da_cidade);
+scanf("%s", &nome_da_cidade1);
 
 printf("Digite a população da cidade: \n");
-scanf("%d", &populacao);
+scanf("%d", &populacao1);
 
 printf("Digite a área da cidade: \n");
-scanf("%f", &area);
+scanf("%f", &area1);
 
 printf("Digite o PIB: \n");
-scanf("%f", &PIB);
+scanf("%f", &PIB1);
 
 printf("Digite a quantidade de pontos turísticos: \n");
-scanf("%d", &pontos_turisticos);
+scanf("%d", &pontos_turisticos1);
+
+printf("\n--------------------------------\n\n");
+
+printf(">>Insira os dados da CARTA 2:\n");
+printf("Digite a letra do estado:\n");
+scanf(" %c", &letra_estado2);
+
+printf("Digite o código da Carta: \n");
+scanf("%s", &codigo_da_carta2);
+
+printf("Digite o Nome da cidade: \n");
+scanf("%s", &nome_da_cidade2);
+
+printf("Digite a população da cidade: \n");
+scanf("%d", &populacao2);
+
+printf("Digite a área da cidade: \n");
+scanf("%f", &area2);
+
+printf("Digite o PIB: \n");
+scanf("%f", &PIB2);
+
+printf("Digite a quantidade de pontos turísticos: \n");
+scanf("%d", &pontos_turisticos2);
 
 
 printf("\nCadastro realizado. Confira os dados abaixo: \n");
-printf("- Letra do Estado: %c\n\n", letra_estado);
-printf("- Código da Carta: %s\n\n", codigo_da_carta);
-printf("- Nome da cidade: %s\n\n", nome_da_cidade);
-printf("- População: %d habitantes\n\n", populacao);
-printf("- Área: %.2fKm²\n\n", area);
-printf("- PIB (em reais): R$ %.2f milhões de reais\n\n", PIB);
-printf("- Quantidade de pontos turísticos: %d\n\n", pontos_turisticos);
+printf("\n <<< CARTA 1 >>>\n");
+printf("- Letra do Estado: %c\n\n", letra_estado1);
+printf("- Código da Carta: %s\n\n", codigo_da_carta1);
+printf("- Nome da cidade: %s\n\n", nome_da_cidade1);
+printf("- População: %d habitantes\n\n", populacao1);
+printf("- Área: %.2fKm²\n\n", area1);
+printf("- PIB (em reais): R$ %.2f \n\n", PIB1);
+printf("- Quantidade de pontos turísticos: %d\n\n", pontos_turisticos1);
 
+
+printf("\n <<< CARTA 2 >>>\n");
+printf("- Letra do Estado: %c\n\n", letra_estado2);
+printf("- Código da Carta: %s\n\n", codigo_da_carta2);
+printf("- Nome da cidade: %s\n\n", nome_da_cidade2);
+printf("- População: %d habitantes\n\n", populacao2);
+printf("- Área: %.2fKm²\n\n", area2);
+printf("- PIB (em reais): R$ %.2f \n\n", PIB2);
+printf("- Quantidade de pontos turísticos: %d\n\n", pontos_turisticos2);
 
 return 0;
 }
