53349864e1af6060d458bf1ce159210b830962e9
andersengollo-dotcom <andersengollo@gmail.com>
2025-09-13 20:19:23 +0000

    Desafio super trunfo intermediário

--- super_trunfo.c
+++ super_trunfo.c
@@ -9,7 +9,8 @@ int populacao1;
 float area1;
 float PIB1;
 int pontos_turisticos1;
-
+float densidadepop1;
+float pibpercapta1;
 
 char letra_estado2;
 char codigo_da_carta2[50];
@@ -18,6 +19,8 @@ int populacao2;
 float area2;
 float PIB2;
 int pontos_turisticos2;
+float densidadepop2;
+float pibpercapta2;
 
 printf(">>Insira os dados da CARTA 1:\n");
 printf("Digite a letra do estado:\n");
@@ -65,6 +68,11 @@ scanf("%f", &PIB2);
 printf("Digite a quantidade de pontos turísticos: \n");
 scanf("%d", &pontos_turisticos2);
 
+pibpercapta1 = (float) PIB1 / populacao1;
+pibpercapta2 = (float) PIB2 / populacao2;
+densidadepop1 = (float) populacao1 / area1;
+densidadepop2 = (float) populacao2 / area2;
+
 
 printf("\nCadastro realizado. Confira os dados abaixo: \n");
 printf("\n <<< CARTA 1 >>>\n");
@@ -75,6 +83,8 @@ printf("- População: %d habitantes\n\n", populacao1);
 printf("- Área: %.2fKm²\n\n", area1);
 printf("- PIB (em reais): R$ %.2f \n\n", PIB1);
 printf("- Quantidade de pontos turísticos: %d\n\n", pontos_turisticos1);
+printf("- Densidade populacional: %.2fhab/km²\n\n", densidadepop1);
+printf("- PIB Per Capta: R$ %.2f\n\n", pibpercapta1);
 
 
 printf("\n <<< CARTA 2 >>>\n");
@@ -85,6 +95,9 @@ printf("- População: %d habitantes\n\n", populacao2);
 printf("- Área: %.2fKm²\n\n", area2);
 printf("- PIB (em reais): R$ %.2f \n\n", PIB2);
 printf("- Quantidade de pontos turísticos: %d\n\n", pontos_turisticos2);
+printf("- Densidade populacional: %.2f hab/km²\n\n", densidadepop2);
+printf("- PIB Per Capta: R$ %.2f\n\n", pibpercapta2);
+
 
 return 0;
 }
